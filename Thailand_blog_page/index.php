9b1625c43992d172c0a44ed47c0c75af9998c2c6
sujeet <sujeet@justgoweb.com>
2024-02-26 17:56:11 +0530

    adding code to library

--- Thailand_blog_page/index.php
+++ Thailand_blog_page/index.php
@@ -389,21 +389,3 @@ get_header(); ?>
 </script>
 
 <?php get_footer(); ?>
-
-// code should be paste in function.php
-function load_categories_into_acf_field($field) {
-    
-    if ($field['type'] == 'select') {
-        $categories = get_categories(array('hide_empty' => 0));
-        $field['choices'] = array('' => 'None'); // Optional: add a 'None' option
-
-        foreach ($categories as $category) {
-           
-            $field['choices'][$category->name] = $category->name;
-        }
-    }
-
-    return $field;
-}
-
-add_filter('acf/load_field/name=blog_category', 'load_categories_into_acf_field');
